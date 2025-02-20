0e147dc16c3554aef1e46c42eef3579298a042fa
perappu <stokoriel@gmail.com>
2025-02-20 17:16:58 -0600

    and now we wrangle vercel

--- src/_data/lorekeeper.js
+++ src/_data/lorekeeper.js
@@ -2,8 +2,8 @@ import { parseFeed } from '@rowanmanning/feed-parser';
 import { DateTime } from 'luxon';
 import { JSDOM } from "jsdom"
 import feeds from "./urls.json" with { type: "json" };
-import puppeteer from 'puppeteer';
 import { Cluster } from 'puppeteer-cluster';
+import chromium from "@sparticuz/chromium-min";
 
 global.DOMParser = new JSDOM().window.DOMParser;
 
@@ -29,8 +29,6 @@ export async function lorekeeper() {
     const items = [];
     const screenshots = [];
 
-    const browser = await puppeteer.launch({ headless: true });
-
     for (let i = 0; i < feeds.length; i++) {
         const response = await fetch(feeds[i]);
         const feed = parseFeed(await response.text());
@@ -40,9 +38,16 @@ export async function lorekeeper() {
         });
     }
 
+    const chromiumPack = "https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v121.0.0-pack.tar";
+
     const cluster = await Cluster.launch({
         concurrency: Cluster.CONCURRENCY_CONTEXT,
         maxConcurrency: 2,
+        puppeteerOptions: {
+            args: chromium.args,
+            executablePath: await chromium.executablePath(chromiumPack),
+            headless: true,
+        }
     });
 
     await cluster.task(async ({ page, data: url }) => {
@@ -64,8 +69,6 @@ export async function lorekeeper() {
     });
 
     items.sort((a, b) => (new Date(b.date)) - (new Date(a.date)));
-
-    await browser.close();
 
     return items;
 
