29151624e2c5665ded617773191442641436f1c3
perappu <stokoriel@gmail.com>
2025-02-20 18:01:10 -0600

    Revert "and now we wrangle vercel"
    This reverts commit 0e147dc16c3554aef1e46c42eef3579298a042fa.

--- src/_data/lorekeeper.js
+++ src/_data/lorekeeper.js
@@ -2,6 +2,8 @@ import { parseFeed } from '@rowanmanning/feed-parser';
 import { DateTime } from 'luxon';
 import {JSDOM} from "jsdom"
 import feeds from "./urls.json" with { type: "json" };
+import puppeteer from 'puppeteer';
+import { Cluster } from 'puppeteer-cluster';
 
 global.DOMParser = new JSDOM().window.DOMParser;
 
@@ -26,16 +28,43 @@ export  async function lorekeeper() {
 
     const items = [];
 
-    for(let i=0; i<feeds.length; i++) {
+    const browser = await puppeteer.launch({ headless: true });
+
+    for (let i = 0; i < feeds.length; i++) {
         const response = await fetch(feeds[i]);
         const feed = parseFeed(await response.text());
         feed.items.forEach(f => {
             items.push(normalizeItem(f, feed));
         });
-	}
+    }
+
+    const cluster = await Cluster.launch({
+        concurrency: Cluster.CONCURRENCY_CONTEXT,
+        maxConcurrency: 2,
+    });
+
+    await cluster.task(async ({ page, data: url }) => {
+        await page.goto(url);
+        page.setViewport({ width: 1920, height: 1080 });
+        const screen = await page.screenshot({ encoding: "base64" });
+        screenshots.push(screen);
+    });
+
+    items.forEach(item => {
+        cluster.queue(item['url']);
+    });
+
+    await cluster.idle();
+    await cluster.close();
+
+    items.forEach(function(item, i) {
+        item['screenshot'] = screenshots[i];
+    });
 
     items.sort((a, b) => (new Date(b.date)) - (new Date(a.date)));
 
-	return items;
-	
+    await browser.close();
+
+    return items;
+
 };
