1c88de68cff341c214b273b35521b19da89e7120
perappu <stokoriel@gmail.com>
2025-02-21 10:20:13 -0600

    lets try it

--- src/_data/lorekeeper.js
+++ src/_data/lorekeeper.js
@@ -1,8 +1,53 @@
 import { parseFeed } from '@rowanmanning/feed-parser';
 import {JSDOM} from "jsdom";
-import puppeteer from 'puppeteer';
 import { Cluster } from 'puppeteer-cluster';
 import * as fs from 'node:fs';
+import chromium from '@sparticuz/chromium';
+
+chromium.setGraphicsMode = false;
+
+const args = [
+    // '--user-data-dir=./tmp', // enable to run locally and disable the below
+    '--user-data-dir=/home/moif/storage', // created in index.js, guess cache folder ends up inside too.
+    "--single-process", // Disable to run locally
+    "--no-sandbox",
+    "--disable-gpu",
+    "--disable-setuid-sandbox",
+    "--no-first-run",
+    '--headless',
+    '--hide-scrollbars',
+    '--mute-audio',
+    '--disable-gl-drawing-for-tests',
+    '--disable-canvas-aa', // Disable antialiasing on 2d canvas
+    '--disable-2d-canvas-clip-aa', // Disable antialiasing on 2d canvas clips
+    '--disable-dev-shm-usage', // ???
+    '--no-zygote', // wtf does that mean ?
+    '--disable-webgl',
+    '--disable-infobars',
+    '--disable-breakpad',
+    '--deterministic-fetch',
+    '--aggressive-cache-discard',
+    '--disable-cache',
+    '--disable-application-cache',
+    '--disable-offline-load-stale-cache',
+    '--disable-gpu-shader-disk-cache',
+    '--media-cache-size=0',
+    '--disk-cache-size=0',
+    '--disable-extensions',
+    '--disable-component-extensions-with-background-pages',
+    '--disable-default-apps',
+    '--no-default-browser-check',
+    '--autoplay-policy=user-gesture-required',
+    '--disable-background-timer-throttling',
+    '--disable-backgrounding-occluded-windows',
+    '--disable-notifications',
+    '--disable-background-networking',
+    '--disable-component-update',
+    '--disable-domain-reliability',
+    '--disable-sync',
+    '--disable-stack-profiler',
+    '--disable-features=PersistentHistograms'
+  ];
 
 const feeds = JSON.parse(fs.readFileSync('./src/_data/urls.json'));
 
@@ -46,8 +91,11 @@ export  async function lorekeeper() {
     const cluster = await Cluster.launch({
         concurrency: Cluster.CONCURRENCY_CONTEXT,
         maxConcurrency: 2,
-        puppeteerOptions:
-        {ignoreDefaultArgs: ['--disable-extensions']}
+        puppeteerOptions: {
+            headless: false,
+            args: args,
+            executablePath: await chromium.executablePath("https://f004.backblazeb2.com/file/perappu-public/chromium-v132.0.0-pack.tar")
+          },
     });
 
     await cluster.task(async ({ page, data: url }) => {
