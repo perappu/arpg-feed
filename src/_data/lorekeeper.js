13cc2b248c057d0f11432346d9cf56120b81abdc
perappu <stokoriel@gmail.com>
2025-02-20 18:00:30 -0600

    Revert "add screenshots"
    This reverts commit 3b49d8847c292d0d1d5c5c7f3ec284ee47dc0276.

--- src/_data/lorekeeper.js
+++ src/_data/lorekeeper.js
@@ -1,9 +1,7 @@
 import { parseFeed } from '@rowanmanning/feed-parser';
 import { DateTime } from 'luxon';
-import { JSDOM } from "jsdom"
+import {JSDOM} from "jsdom"
 import feeds from "./urls.json" with { type: "json" };
-import { Cluster } from 'puppeteer-cluster';
-import chromium from "@sparticuz/chromium-min";
 
 global.DOMParser = new JSDOM().window.DOMParser;
 
@@ -14,55 +12,30 @@ function removeHTMLTags(html) {
 
 function normalizeItem(item, feed) {
     return {
-        'origin': feed.title,
+        'origin' : feed.title,
         // LK doesn't give us the origin url for some reason
-        'site': feed.self.split('/')[2],
-        'url': item.url,
-        'title': item.title,
-        'date': item.updated,
-        'description': removeHTMLTags(item.description).substring(0, 500)
+        'site' : feed.self.split('/')[2],
+        'url' : item.url,
+        'title' : item.title,
+        'date' : item.updated,
+        'description' : removeHTMLTags(item.description).substring(0,400)
     }
 }
 
-export async function lorekeeper() {
+export  async function lorekeeper() {
 
     const items = [];
-    const screenshots = [];
 
-    for (let i = 0; i < feeds.length; i++) {
+    for(let i=0; i<feeds.length; i++) {
         const response = await fetch(feeds[i]);
         const feed = parseFeed(await response.text());
         feed.items.forEach(f => {
-            var item = normalizeItem(f, feed);
-            items.push(item);
+            items.push(normalizeItem(f, feed));
         });
-    }
-
-    const cluster = await Cluster.launch({
-        concurrency: Cluster.CONCURRENCY_CONTEXT,
-        maxConcurrency: 2,
-    });
-
-    await cluster.task(async ({ page, data: url }) => {
-        await page.goto(url);
-        page.setViewport({ width: 1920, height: 1080 });
-        const screen = await page.screenshot({ encoding: "base64" });
-        screenshots.push(screen);
-    });
-
-    items.forEach(item => {
-        cluster.queue(item['url']);
-    });
-
-    await cluster.idle();
-    await cluster.close();
-
-    items.forEach(function(item, i) {
-        item['screenshot'] = screenshots[i];
-    });
+	}
 
     items.sort((a, b) => (new Date(b.date)) - (new Date(a.date)));
 
-    return items;
-
+	return items;
+	
 };
