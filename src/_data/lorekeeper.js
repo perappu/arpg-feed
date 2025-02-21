077b14f9c58d364baa3fd519ad62ebb14414e68b
perappu <stokoriel@gmail.com>
2025-02-21 08:44:30 -0600

    Revert "and now we wrangle vercel"
    This reverts commit 0e147dc16c3554aef1e46c42eef3579298a042fa.

--- src/_data/lorekeeper.js
+++ src/_data/lorekeeper.js
@@ -1,6 +1,7 @@
 import { parseFeed } from '@rowanmanning/feed-parser';
 import {JSDOM} from "jsdom"
 import feeds from "./urls.json" with { type: "json" };
+import puppeteer from 'puppeteer';
 import { Cluster } from 'puppeteer-cluster';
 
 global.DOMParser = new JSDOM().window.DOMParser;
@@ -25,6 +26,8 @@ function normalizeItem(item, feed) {
 export  async function lorekeeper() {
 
     const items = [];
+
+    const browser = await puppeteer.launch({ headless: true });
 
     for (let i = 0; i < feeds.length; i++) {
         const response = await fetch(feeds[i]);
@@ -58,6 +61,8 @@ export  async function lorekeeper() {
     });
 
     items.sort((a, b) => (new Date(b.date)) - (new Date(a.date)));
+
+    await browser.close();
 
     return items;
 
