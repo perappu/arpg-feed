5c11f55db6b4894dbb7b3083a60c563c232fc9db
perappu <stokoriel@gmail.com>
2025-02-21 14:32:26 -0600

    ok this is cleaner anyway

--- src/_data/libs/getScreenshot.js
+++ src/_data/libs/getScreenshot.js
@@ -1,7 +1,50 @@
 import chromium from '@sparticuz/chromium-min';
 import puppeteer from 'puppeteer-core';
 
-const getScreenshot = async (url) => {
+const args = [
+    // '--user-data-dir=./tmp', // enable to run locally and disable the below
+    '--user-data-dir=/home/moif/storage', // created in index.js, guess cache folder ends up inside too.
+    "--single-process", // Disable to run locally
+    "--no-sandbox",
+    "--disable-gpu",
+    "--disable-setuid-sandbox",
+    "--no-first-run",
+    '--headless',
+    '--hide-scrollbars',
+    '--mute-audio',
+    '--disable-gl-drawing-for-tests',
+    '--disable-canvas-aa', // Disable antialiasing on 2d canvas
+    '--disable-2d-canvas-clip-aa', // Disable antialiasing on 2d canvas clips
+    '--disable-dev-shm-usage', // ???
+    '--no-zygote', // wtf does that mean ?
+    '--disable-webgl',
+    '--disable-infobars',
+    '--disable-breakpad',
+    '--deterministic-fetch',
+    '--aggressive-cache-discard',
+    '--disable-cache',
+    '--disable-application-cache',
+    '--disable-offline-load-stale-cache',
+    '--disable-gpu-shader-disk-cache',
+    '--media-cache-size=0',
+    '--disk-cache-size=0',
+    '--disable-extensions',
+    '--disable-component-extensions-with-background-pages',
+    '--disable-default-apps',
+    '--no-default-browser-check',
+    '--autoplay-policy=user-gesture-required',
+    '--disable-background-timer-throttling',
+    '--disable-backgrounding-occluded-windows',
+    '--disable-notifications',
+    '--disable-background-networking',
+    '--disable-component-update',
+    '--disable-domain-reliability',
+    '--disable-sync',
+    '--disable-stack-profiler',
+    '--disable-features=PersistentHistograms'
+  ];
+
+export async function getScreenshot(url) {
     console.log(url);
     const browser = await puppeteer.launch({
         args: args,
@@ -18,5 +61,3 @@ const getScreenshot = async (url) => {
 
     return screen;
 };
-
-module.exports = getScreenshot;
--- src/_data/lorekeeper.js
+++ src/_data/lorekeeper.js
@@ -1,50 +1,7 @@
 import { parseFeed } from '@rowanmanning/feed-parser';
 import {JSDOM} from "jsdom";
 import * as fs from 'node:fs';
-import getScreenshot from './libs/getScreenshot.js';
-
-const args = [
-    // '--user-data-dir=./tmp', // enable to run locally and disable the below
-    '--user-data-dir=/home/moif/storage', // created in index.js, guess cache folder ends up inside too.
-    "--single-process", // Disable to run locally
-    "--no-sandbox",
-    "--disable-gpu",
-    "--disable-setuid-sandbox",
-    "--no-first-run",
-    '--headless',
-    '--hide-scrollbars',
-    '--mute-audio',
-    '--disable-gl-drawing-for-tests',
-    '--disable-canvas-aa', // Disable antialiasing on 2d canvas
-    '--disable-2d-canvas-clip-aa', // Disable antialiasing on 2d canvas clips
-    '--disable-dev-shm-usage', // ???
-    '--no-zygote', // wtf does that mean ?
-    '--disable-webgl',
-    '--disable-infobars',
-    '--disable-breakpad',
-    '--deterministic-fetch',
-    '--aggressive-cache-discard',
-    '--disable-cache',
-    '--disable-application-cache',
-    '--disable-offline-load-stale-cache',
-    '--disable-gpu-shader-disk-cache',
-    '--media-cache-size=0',
-    '--disk-cache-size=0',
-    '--disable-extensions',
-    '--disable-component-extensions-with-background-pages',
-    '--disable-default-apps',
-    '--no-default-browser-check',
-    '--autoplay-policy=user-gesture-required',
-    '--disable-background-timer-throttling',
-    '--disable-backgrounding-occluded-windows',
-    '--disable-notifications',
-    '--disable-background-networking',
-    '--disable-component-update',
-    '--disable-domain-reliability',
-    '--disable-sync',
-    '--disable-stack-profiler',
-    '--disable-features=PersistentHistograms'
-  ];
+import { getScreenshot } from './libs/getScreenshot.js';
 
 const feeds = JSON.parse(fs.readFileSync('./src/_data/urls.json'));
 
